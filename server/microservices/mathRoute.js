cc15b1e4cf41f4541e8c47df1f0f90e169abaadb
S. Vikram <svikram@analytica-india.com>
2017-03-01 13:24:34 -0800

    Some minimal code cleanup for microservices sample implementation.

--- server/microservices/mathRoute.js
+++ server/microservices/mathRoute.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
+   var seneca = require('seneca')();
 
    app.get('/math/add/:p1/:p2', function(msg, resp) {
-      require('seneca')()
-      .use('math')
+      seneca.use('math')
       .act({role:'web',cmd:'add',p1:msg.params.p1,p2:msg.params.p2}, 
          function(err, result) {
             if(err) console.error(err);
